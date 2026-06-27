4048853c9b2fd138b27434ab6e30a0ab281e8da3
Vitora OS <emmykarl07@gmail.com>
2026-06-27 18:41:13 +0100

    Initialize PrismaClient with development configuration

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,1 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+import { config } from '../config.js'
 
+const isDev = config.NODE_ENV === 'development'
+
+// In development, attach to globalThis so hot reloads don't create new
+// PrismaClient instances and exhaust the connection pool.
+const globalForPrisma = globalThis
+
+function createPrismaClient() {
+  return new PrismaClient({
+    log: isDev
+      ? ['query', 'warn', 'error']
+      : ['error'],
+  })
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient()
+
+if (isDev) {
+  globalForPrisma.prisma = prisma
+}
